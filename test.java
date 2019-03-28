2312ed50258ef550c2677034b77b85d826aac7ef
Monokumasama <47858126+Monokumasama@users.noreply.github.com>
2019-03-28 17:53:38 +0100

    Add files via upload

---
+++ test.java
@@ -0,0 +1,39 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class test {
+	public test() {
+		
+		
+	}
+	   String tab_toString(int[] tab) {
+				String chaine = new String();
+				for (int i : tab) {
+					chaine += " "+ i;
+				}
+				return chaine;
+			}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test tata  = new test();
+		Board plateau = new Board(3,2,2,8,5);
+		System.out.println("PLATEAU ACTUEL" + "\n");
+	    System.out.println(plateau.toString());
+		Nextsituations nextsituations = new Nextsituations(plateau.gamers[0], plateau) ;
+		System.out.println("\n PROCHAINS PLATEAUX POSSIBLES À L'ISSUE DU TOUR DU JOUEUR"+ "\n");
+		System.out.println(nextsituations.toString_boards());
+		Actions_graded actions_notées = new Actions_graded( nextsituations, plateau.gamers[1]);
+		System.out.println("PROCHAINES ACTIONS POSSIBLES DU JOUEUR "+ "\n");
+		System.out.println(actions_notées.toString());
+		IA bien_basique  = new IA();
+		int[] choix = bien_basique.IA_basique(actions_notées);
+		System.out.println("ACTION RETENUE"+ "\n");
+		System.out.println(tata.tab_toString(choix));
+	}
+
+}
